ac4d2cf39b5e22a039c798a8b6f8036b00db4aed
Camila <camilaabalova@gmail.com>
2023-04-26 18:27:35 +0300

    dependencies updates

--- app/src/main/java/com/ca/chat/MainActivity.kt
+++ app/src/main/java/com/ca/chat/MainActivity.kt
@@ -3,7 +3,6 @@ package com.ca.chat
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -33,8 +32,6 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun Greeting(name: String) {
     Text(
-        modifier = Modifier
-            .clickable { throw RuntimeException("Test Crash") },
         text = "Hello $name!"
     )
 }
